edabffa90afd9a0446bbe32be0286b4bdb35ea83
liambarry7 <74929060+liambarry7@users.noreply.github.com>
2025-11-19 23:43:11 +0000

    Create ml.py

---
+++ ml.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Practical assignment on supervised and unsupervised learning
+Coursework 002 for: CMP-6058A Artificial Intelligence
+
+Includes a State class for Task 1
+
+@author: Liam
+@date:   19/11/2025
+
+"""
+
+'''
+    TO DO:
+        PART 2A
+        - Google MediaPipe for image feature extraction and data labelling
+        
+        PART 2B
+        Pre-proccessing of dataset
+        - clean data
+        - apply any other pre-proccessing steps necessary
+        - explain why and compare before and after of data distribution
+        
+        PART 2C
+        - Decision tree
+        - kNN (standard libraries only)
+        
+        
+        
