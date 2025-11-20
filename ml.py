fe3dc953832d40d606f23ab591ea5a0da4487815
liambarry7 <liambarry406@yahoo.co.uk>
2025-11-20 00:12:55 +0000

    added more to todo list

--- ml.py
+++ ml.py
@@ -5,13 +5,17 @@ Coursework 002 for: CMP-6058A Artificial Intelligence
 
 Includes a State class for Task 1
 
-@author: Liam
+@author: Liam, James
 @date:   19/11/2025
 
 """
 
 '''
     TO DO:
+        PART 1
+        Data collection - individual
+        
+        
         PART 2A
         - Google MediaPipe for image feature extraction and data labelling
         
@@ -22,8 +26,39 @@ Includes a State class for Task 1
         - explain why and compare before and after of data distribution
         
         PART 2C
+        Supervised learning, optimising, evaluation and comparing classifiers
         - Decision tree
         - kNN (standard libraries only)
+        - a third classifier of our choice
+        - split dataset into training and test sets
+        - justify and explain strategy for splitting the data
+        - optimise and compare classifiers
+            - fine tune at least 2 common hyperparameters, using 5-fold cross validation
+            - use a range of performance metrics to assess modesl (e.g. accuracy, sensitivity)
+            - choose the best model across all hyperparameter settings and explain why
+            - assess best model on whole training set w/ best hyperparameters - report test accuracy and 
+              compare it to classifier performance
+            - create tables, plots, confusion matrix to report key information, like class distribution
+              performance at aech hyperparameter setting and best models performance on training and test sets
+            - present and effectively discuss the process you followed to optimise the classifiers - effectively 
+              compare the performance of them and justify the best one
+              
+        PART 2D
+        Unsupervised learning and clustering data
+        - remove class labels from dataset and apply clustering algorithsm
+        - e.g. use K-means or hierarchical clustering
+        - analyse clusters formed, report effectiveness of each clustering method
+        - compare clustering outputs with best classifier models and report findings
+        
+        PART 2E
+        Academic poster
+        
+        PART 3
+        Individual written report
+'''
+
+print("Hello World!")
+        
         
         
         
