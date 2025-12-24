11ec28b0d1e7cb1590037690ace14a17020e495a
liambarry7 <74929060+liambarry7@users.noreply.github.com>
2025-12-24 11:27:27 +0000

    added script for classification models

---
+++ models.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Practical assignment on supervised and unsupervised learning
+Coursework 002 for: CMP-6058A Artificial Intelligence
+
+Script containing different classification models
+
+@author: Liam
+@date:   19/12/2025
+
+"""
+
+
+def kNN():
+    print("kNN")
+
+
+def decision_tree():
+    print("decsiion tree")
