d2a5a25735b69f5f68541ec3697e53eb41e5cfbb
chRist8n <152877601+chRist8n@users.noreply.github.com>
2025-12-26 12:28:44 +0000

    kNN implemented - NOT tested yet

--- models.py
+++ models.py
@@ -10,10 +10,47 @@ Script containing different classification models
 
 """
 
+from collections import Counter
+import math
 
-def kNN():
-    print("kNN")
 
+#Chris
+#kNN start
+def euclidean(A, B):
+    return math.sqrt(sum((a - b) ** 2 for a, b in zip(A, B)))
+
+def get_neighbours(dataset, input_vector, k):
+    print("Searching for ", k, " nearest neighbours ...")
+
+    #create and populate a list of distances using euclidean function
+    distances = []
+    for features, handsign in dataset:
+        dist = euclidean(features, input_vector)
+        distances.append(dist, handsign)
+    
+    #sort by lowest distance first
+    distances.sort(key=lambda x: x[0])
+    #take the first k sorted elements and assign to 'neighbours'
+    neighbours = distances[:k]
+
+    return neighbours
+
+def kNN_predict(dataset, input_vector, k):
+    neighbours = get_neighbours(dataset, input_vector, k)
+
+    #create a list of just the labels
+    labels = (label for _, labels in neighbours)
+    #list in order of most common 
+    #(in tuple format - eg [A, k] --> take first element of first tuple)
+    most_common_label = Counter(labels).most_common(1)[0][0]
+
+    return most_common_label
+
+def kNN(dataset, input_vector, k):
+    print("kNN --> k = ", k)
+    print(kNN_predict(dataset, input_vector, k))
+
+#kNN end
 
 def decision_tree():
     print("decsiion tree")
