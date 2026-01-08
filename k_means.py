90f02e59deb4bef7789c2604c9f8433291042a7b
jamesbar54 <149095651+jamesbar54@users.noreply.github.com>
2026-01-08 17:37:59 +0000

    completed k_means

--- k_means.py
+++ k_means.py
@@ -7,97 +7,102 @@ Created on Wed Jan  7 15:43:52 2026
 
 from data_handling import dataset_split
 import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
 import random
+from scipy import stats
 
 
 def euclidean(a, b):
     '''Give euclidean distance of 2 points a and b'''
     
     return np.sqrt(np.sum((a - b) ** 2, axis =1))
+    
+def k_mean(X, y, k):
+    
+    cluster = KMeans(n_clusters=k).fit(X)
+    
+    predictions = cluster.predict(X)
+    
+    centroids = cluster.cluster_centers_ 
+    
+    
+    data_map = []
+    for d in X:
+        e = euclidean(d, centroids)
+        centroid = np.argmin(e)
+        data_map.append(centroids[centroid])
+        
+    centroid_classifiaction = []
+    
+    for c in centroids:
+        letter = []
+        for i in range(len(data_map)):
+            if (c == data_map[i]).all():
+                letter.append(y[i])
+        centroid_classifiaction.append([c, stats.mode(letter)[0]])
+    
+    return centroid_classifiaction
 
-def kmeans(data, y, k, max_iter):
-    
-    min_range = -5
-    max_range = 5
-    
-    #set random datapoints for inital centoids
+
+def find_k_means(cluster, item):
     centroids = []
     
-    for i in range(k):
-        centroid = []
-        
-        for i in range(len(data[0])):
-            centroid.append(random.uniform(min_range, max_range))
-        
-        centroids.append(centroid)
-            
-    print(centroids)
+    for c in cluster:
+        centroids.append(c[0])
     
-    #set up loop
-    itera = 0
-    prev_centroids = None
-    changed = True
+    signs = []
     
-    print("before loop")
+    for i in item:
+        e = euclidean(i, centroids)
+        centroid = np.argmin(e)
+        signs.append(cluster[centroid][1])
     
-    #loop through
-    while changed and itera < max_iter:
-        new_cluster = []
-        for i in range(k):
-            new_cluster.append([])
-            
-        for i in data:
-            distance = euclidean(i, centroids)
-            nearest = np.argmin(distance)
-            new_cluster[nearest].append(i)
+    return signs
+    
 
-        prev_centroids = centroids.copy()
-        for i in range(len(new_cluster)):
-            if len(new_cluster[i]) == 0:
-                cluster_centroid = np.zeros(len(data[0]))
-            else: 
-                cluster_centroid = np.mean(new_cluster[i], axis=0)
-            centroids[i] = cluster_centroid
-        
-        itera += 1
-        changed = np.any(np.not_equal(centroids, prev_centroids))
+def find_k(data, start):
     
-    print("after loop")
-    print(centroids)
-    print(itera)
+    k = []
+    loss = []
+    offset = 20
     
-    #need to fix mapping
-# =============================================================================
-#     closest = []
-#     for point in data:
-#         distance = euclidean(point, centroids)
-#         nearest = np.argmin(distance)
-#         closest.append(centroids[nearest])
-#     
-#     closest = np.array(closest)
-#     
-#     data_map = []    
-#     for i in range(k):
-#         data_map.append([])
-# =============================================================================
+    for i in range(50):
+        print(i)
+        cluster = k_mean(data, start + i*offset)
+        k.append(i + i*offset)
+        loss.append(cluster.inertia_)
     
     
-    
+    plt.figure()
+    plt.plot(k, loss)
+
 
 
 def tester():
     
     training_set, test_set = dataset_split()
-
-    # split training set into x (landmarks) and y (labels)
     x_train = training_set.drop(['Encoded_sign'], axis=1).to_numpy()  # axis = 0  -> operate along rows, axis = 1  -> operate along columns
     y_train = training_set['Encoded_sign'].to_numpy()
     
-    #b_data = np.array([5.1,4.5])
+    mean = k_mean(x_train, y_train, 175)
     
-    #print(euclidean(data, b_data))
+    item = test_set.drop(['Encoded_sign'], axis=1).to_numpy()
+    answers = test_set['Encoded_sign'].to_numpy()
     
-    kmeans(x_train, y_train, 10, 100)
+    values = find_k_means(mean, item)
+    
+    count = 0
+    for i in range(len(values)):
+        print(values[i])
+        if (values[i] == answers[i]).all():
+            count += 1
+    
+    print(count, "/", len(answers))
+        
+    
+    
+
     
 if __name__ == '__main__':
     tester()
