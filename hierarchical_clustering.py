1b3570700a7e6153aef4f66c1077ae6c078f531b
liambarry7 <74929060+liambarry7@users.noreply.github.com>
2026-01-09 14:24:45 +0000

    removed old agglomerative, added dendrogram to new one

--- hierarchical_clustering.py
+++ hierarchical_clustering.py
@@ -1,56 +1,43 @@
+from matplotlib import pyplot as plt
+from scipy.cluster.hierarchy import linkage, dendrogram
+
 from data_handling import dataset_split
 from sklearn.cluster import AgglomerativeClustering
-from scipy.cluster.hierarchy import linkage, dendrogram
-import matplotlib.pyplot as plt
-from sklearn import metrics
+from sklearn.metrics import accuracy_score
+import numpy as np
+from collections import Counter
 
 
-def agglomerative_clustering(x, y, n_c, metric):
-    print("Hierarchical Clustering")
-
-    agglomerative_cluster = AgglomerativeClustering(n_clusters= 2, metric= 'euclidean', linkage='ward')
-    # agglomerative_cluster.
-
-def test(training_data, features):
-    # https://www.geeksforgeeks.org/machine-learning/hierarchical-clustering/
-    # https://www.geeksforgeeks.org/machine-learning/implementing-agglomerative-clustering-using-sklearn/
-    # https://www.geeksforgeeks.org/machine-learning/hierarchical-clustering-with-scikit-learn/
-    # https://developer.ibm.com/tutorials/awb-implement-hierarchical-clustering-python/
-    # https://www.datacamp.com/tutorial/introduction-hierarchical-clustering-python
-
-    complete_clustering = linkage(training_data, method="ward", metric="euclidean")
-    dendrogram(complete_clustering, color_threshold=15)
-    plt.show()
-
-    hierarchical_cluster = AgglomerativeClustering(n_clusters=10, linkage="ward")
-
-    labels = hierarchical_cluster.fit_predict(training_data) # what cluster each row has been put into
-    print(labels)
-    print(labels.shape)
-    print(features.shape)
-
-    if len(labels) == len(features):
-        for i in range(len(labels)):
-            print(labels[i], features[i]) # print cluster and actual class
-
-    print(metrics.accuracy_score(features, labels) * 100)
-    print(metrics.adjusted_rand_score(features, labels) * 100)
-    print(metrics.silhouette_score(training_data, labels) * 100)
-
-
-
-def tester():
+def agglomerative_clustering():
+    # get dataset
     training_set, test_set = dataset_split()
     x_train = training_set.drop(['Encoded_sign'], axis=1).to_numpy()
     y_train = training_set['Encoded_sign'].to_numpy()
 
-    training_data = training_set.drop(['Encoded_sign'], axis=1).to_numpy()
+    # display dendrogram
+    complete_clustering = linkage(x_train, method="ward", metric="euclidean")
+    dendrogram(complete_clustering, color_threshold=15)
+    plt.show()
 
+    # create hierarchical clustering object (agglomerative)
+    hc = AgglomerativeClustering(n_clusters=10, metric='euclidean', linkage='ward')
+    y_clusters = hc.fit_predict(x_train)
 
+    # map each cluster to an estimated label (most common in that cluster)
+    cluster_to_label = {}
+    for cluster_id in np.unique(y_clusters):
+        indices = np.where(y_clusters == cluster_id)
+        true_labels = y_train[indices]
+        cluster_to_label[cluster_id] = Counter(true_labels).most_common(1)[0][0]
 
-    # agglomerative_clustering(x_train, y_train)
-    test(x_train, y_train)
+    # populate y_pred with predicted labels for each cluster
+    y_pred = np.array([cluster_to_label[c] for c in y_clusters])
 
+    accuracy = accuracy_score(y_train, y_pred)
+    print(f"Hierarchical clustering accuracy: {accuracy * 100:.2f}%")
+
+def test_harness():
+    agglomerative_clustering()
 
 if __name__ == '__main__':
-    tester()
+    test_harness()
